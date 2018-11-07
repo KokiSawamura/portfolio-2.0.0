8737f9f913ff255f0933f7ad82489223c1c4c5bd
Sawamura, Koki a | D | CARDB <koki.a.sawamura@rakuten.com>
2018-11-07 09:14:01 +0900

    no message

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,14 @@
       section : ".scrollify-section",
       interstitialSection: 'header, footer',
       scrollbars: false,
-      updateHash: false
+      updateHash: false,
+      before: function(i,arr) {
+        var ref = panels[i].attr("href");
+
+        $(".pagination .active").removeClass("active");
+
+        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+      }
     });
   });
 })(jQuery);
